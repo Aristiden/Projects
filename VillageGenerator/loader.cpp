fec65e0ae14c066c653f6c2f71f9235f1527e0b8
Robert Wolle <31664927+Aristiden@users.noreply.github.com>
2025-08-18 10:20:28 -0400

    Create loader.cpp

---
+++ VillageGenerator/loader.cpp
@@ -0,0 +1,34 @@
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+// DOCUMENTATION of stb_image.h
+//
+// Limitations:
+//    - no 12-bit-per-channel JPEG
+//    - no JPEGs with arithmetic coding
+//    - GIF always returns *comp=4
+//
+// Basic usage (see HDR discussion below for HDR usage):
+//    int x,y,n;
+//    unsigned char *data = stbi_load(filename, &x, &y, &n, 0);
+//    // ... process data if not NULL ...
+//    // ... x = width, y = height, n = # 8-bit components per pixel ...
+//    // ... replace '0' with '1'..'4' to force that many components per pixel
+//    // ... but 'n' will always be the number that it would have been if you said 0
+//    stbi_image_free(data);
+//
+// Standard parameters:
+//    int *x                 -- outputs image width in pixels
+//    int *y                 -- outputs image height in pixels
+//    int *channels_in_file  -- outputs # of image components in image file
+//    int desired_channels   -- if non-zero, # of image components requested in result
+
+// To query the width, height and component count of an image without having to
+// decode the full file, you can use the stbi_info family of functions:
+//
+//   int x,y,n,ok;
+//   ok = stbi_info(filename, &x, &y, &n);
+//   // returns ok=1 and sets x, y, n if image is a supported format,
+//   // 0 otherwise.
+
+unsigned char *data = stbi_load("village_line.jpg", &x, &y, &n, 0);
