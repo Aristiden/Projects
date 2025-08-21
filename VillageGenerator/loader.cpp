3b76b771b45ea0aff11d4d6313b55b244ee92381
Robert Wolle <31664927+Aristiden@users.noreply.github.com>
2025-08-21 03:45:31 +0000

    Implemented placing multiple buildings

--- VillageGenerator/loader.cpp
+++ VillageGenerator/loader.cpp
@@ -5,6 +5,7 @@
 #include <array>
 #include <cmath>
 #define _USE_MATH_DEFINES
+#include <random>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "ext/stb_image.h"
@@ -12,7 +13,45 @@
 #include "ext/stb_image_write.h"
 
 
+
+// DEBUG FUNCTIONS
+
+void doesItMakeIt(int iter = 0) {
+    std::cout << "yep" << std::endl;
+    if (iter > 0) {
+        std::cout << "yep, " << iter << std::endl;
+    }
+}
+
+void intPrint(int print) {
+    std::cout << print << std::endl;
+}
+
+void floatPrint(float print) {
+    std::cout << print << std::endl;
+}
+
+void stringPrint(std::string print) {
+    std::cout << print << std::endl;
+}
+
+
 // FUNCTION DEFINITIONS
+
+float gaussianRoller(int n) {
+    // Produces a pseudorandom number between 0 and 1 from a pseudo Gaussian centered at 0.5
+    //   More n produces more Gaussian!
+
+    float output = 0;
+    srand(time(0));
+
+    for (int roll = 0; roll < n; roll++) {
+        output += rand() % 6;
+    }
+    output = output/(n*5);
+   
+    return output;
+}
 
 int getPixel(uint8_t *img,int pixel_pos[], int dims[]) {
     // This function takes in an image loaded as a 1D array and a desired pixel position, then returns the grayscale value of that pixel
@@ -26,23 +65,28 @@ int getPixel(uint8_t *img,int pixel_pos[], int dims[]) {
     return pixel;
 }
 
-/*int getHits(uint8_t *img, int dims[]) {
-    // Unnecessary function, but useful if you have to initialize a vector with an explicit length
+std::vector<std::array<int, 2>> getLineFull(uint8_t *img, int dims[]) {
+    // Produces all points along an input line of black points in {(x1,y1),(x2,y2),...} format
 
     int height,width,hits;
     width = dims[0];
     height = dims[1];
     hits = 0;
 
-    for (int j = 0; j< height; j++) {
-        for (int i = 0; i<width; i++) {
+    std::vector<std::array<int,2>> line_points;
+    std::array<int, 2> position = {0,0};
+
+    for (int j = 0; j < height; j++) {
+        for (int i = width-1; i >= 0; i--) {
             if (img[j * width + i] < 128) {
-                hits++;
+                position = {i,j};
+                line_points.push_back(position);
+                 
             }
         }
     }
-    return hits;
-}*/
+    return line_points;
+}
 
 std::vector<std::array<int, 2>> getLinePoints(uint8_t *img, int dims[], int sample) {
     // Produces a sample of points along an input line of black points in {(x1,y1),(x2,y2),...} format
@@ -93,16 +137,49 @@ std::vector<std::array<int, 4>> getPointNormals(std::vector<std::array<int, 2>> 
     return point_normals;
 }
 
-std::array<std::array<int, 2>, 4> drawBuilding(int corner_index, std::array<int,2> ftprint, std::vector<std::array<int, 4>> normals) {
+
+
+std::vector<std::array<int, 4>> flipNormalsVertical (std::vector<std::array<int, 4>> nrmals, int dims[]) {
+    // Takes the normals = {x1,y1, n1,n2} format and flips the vertical coordinates around the middle of the total image
+
+    int height = dims[1]-1;
+
+    for (int s = 0; s < nrmals.size(); s++) {
+        nrmals[s][1] = height - nrmals[s][1];
+        nrmals[s][3] = height - nrmals[s][3];
+    }
+
+    return nrmals;
+}
+
+std::array<std::array<int, 2>, 4> flipBuilding (std::array<std::array<int, 2>, 4> building_corners, int dims[]) {
+    // Takes in an array of 4 (x,y) points and flips all y components around the middle of the total image
+
+    int height = dims[1]-1;
+
+    for (int s = 0; s < 4; s++) {
+        building_corners[s][1] = height - building_corners[s][1];
+    }
+    return building_corners;
+}
+
+
+std::array<std::array<int, 2>, 4> drawBuilding(int corner_index, std::array<int,2> ftprint, std::vector<std::array<int, 4>> normals_list, int dims[]) {
+    // Pixels get counted from top to bottom, so all of my math is upside down. I'm just going to flip it here and then flip it back at the end:
+    std::vector<std::array<int, 4>> nrmals;
+    nrmals = flipNormalsVertical(normals_list, dims);
+    
     std::array<std::array<int, 2>, 4> building_corners;
     // Points are listed counterclockwise
     int length = ftprint[0];
     int depth = ftprint[1];
+    //std::cout << "nrmals[corner_index][2] = " << nrmals[corner_index][2] << std::endl;
 
     // Bottom left corner, cornerstone:
-    int x1 = normals[corner_index][2];
-    int y1 = normals[corner_index][3];
+    int x1 = nrmals[corner_index][2];
+    int y1 = nrmals[corner_index][3];
     building_corners[0] = {x1, y1};
+    
     // Can't actually use x1, y1 for calculations unfortunately because it may need to be changed if the building doesn't fit the road.
 
     std::array<int, 2> test_normal;
@@ -113,40 +190,64 @@ std::array<std::array<int, 2>, 4> drawBuilding(int corner_index, std::array<int,
 
     while (dist < length) {  // Finds another normal to draw the front wall along
         i++;
-        test_normal = {normals[corner_index-i][2], normals[corner_index-i][3]};
+        test_normal = {nrmals[corner_index-i][2], nrmals[corner_index-i][3]};
+
         dist = sqrt(pow(building_corners[0][0]-test_normal[0],2) + pow(building_corners[0][1]-test_normal[1],2)); // Prolly should make this into a function
         dx = test_normal[0] - building_corners[0][0];
         dy = test_normal[1] - building_corners[0][1];
         for(int k = 0; k < i; k++) {
-            test_func = (dy/dx)*(normals[corner_index-k][0] - test_normal[0]) + test_normal[1];
-            if (normals[corner_index - k][1] > test_func) {
+            test_func = (dy/dx)*(nrmals[corner_index-k][0] - test_normal[0]) + test_normal[1];
+            if (nrmals[corner_index - k][1] > test_func) {
+                stringPrint("fails");
                 corner_index--;
-                building_corners[0] = {normals[corner_index][2], normals[corner_index][3]};
-                i = 0;
+                building_corners[0] = {nrmals[corner_index][2], nrmals[corner_index][3]};
+                i = 0, dist = 0;
             }
         }
     }
 
     float theta = atan(dy/dx);
-    int x2 = ceil(length*cos(theta));
-    int y2 = ceil(length*sin(theta));
-    building_corners[1] = {x2, y2};
+    std::cout << "theta = " << theta << std::endl;
+    int x2 = building_corners[0][0] + ceil(length*cos(theta));
+    int y2 = building_corners[0][1] + ceil(length*sin(theta));
+    building_corners[1] = {(int)x2, (int)y2};
 
     int x3 = round(x2 - depth*cos(M_PI_2 - theta));
-    int y3 = round(y2 + depth*cos(M_PI_2 - theta));
-    building_corners[2] = {x3, y3};
+    int y3 = round(y2 + depth*sin(M_PI_2 - theta));
+    building_corners[2] = {(int)x3, (int)y3};
 
     int x4 = round(building_corners[0][0] - depth*cos(M_PI_2 - theta));
-    int y4 = round(building_corners[0][1] - depth*sin(M_PI_2 - theta));
-    building_corners[3] = {x4, y4};
+    int y4 = round(building_corners[0][1] + depth*sin(M_PI_2 - theta));
+    building_corners[3] = {(int)x4, (int)y4};
     
     // Need to: 
     //   - Still draw building along line to neighboring normal point if the next point doesn't intersect (function test(s))
+
+    building_corners = flipBuilding(building_corners, dims);
     return building_corners;
 }
 
+std::vector<std::array<int, 2>> drawManyBuildings(int n_buildings, std::array<int,2> ftprint, std::vector<std::array<int, 4>> normals_list, int dims[]) {
+
+    std::vector<std::array<int, 2>> building_data;
+    std::array<std::array<int, 2>, 4> building;
+    int random_index;
+    srand(time(0));
+    for (int n = 0; n < n_buildings; n++) {
+        //random_index = round(normals_list.size()*gaussianRoller(10));
+        random_index = rand() % normals_list.size();
+        building = drawBuilding(random_index, ftprint, normals_list, dims);
+        for (int s = 0; s < 4; s++) {
+            building_data.push_back(building[s]);
+        }
+    }
+
+    return building_data;
+}
 
 // PRINT FORMATTING/VISUALIZATION FUNCTIONS
+
+
 
 void getPixelPrint(uint8_t *img,int pixel_pos[], int dims[]) {
     // Function that prints the grayscale value of a desired pixel to the terminal (see getPixel)
@@ -208,29 +309,9 @@ uint8_t *arrayToImage(uint8_t *img, std::vector<std::array<int,2>> points, int d
     return img;
 }
 
-/*uint8_t *processImage(uint8_t *img, int dims[]) {
-    int height,width;
-    width = dims[0];
-    height = dims[1];
 
-    for (int j = 0; j< height; j++) {
-        for (int i = 0; i<width; i++) {
-            if (img[j * width + i] < 128) {
-                img[j * width + i] = 1;
-            } else {
-                img[j * width + i] = 0;
-            }
-        }
-    }
-    return img;
-}*/
 
 //---------------------------
-
-// Graveyard:
-    // Prints particular image pixel's value:
-    /*int pixel_position[2] = {223,631};
-    getPixelPrint(image, pixel_position, dimensions)*/
 
 
 
@@ -264,12 +345,86 @@ int main()
     for (int s = 0; s < normals.size(); s++) {
         normals_data.push_back({normals[s][2], normals[s][3]});
     }
-    arrayToImage(sampled_image, normals_data, dimensions, "testNormals.jpg", 1, quality, blank);
+    // printPoints(normals_data);
+    uint8_t *normals_image = arrayToImage(sampled_image, normals_data, dimensions, "testNormals.jpg", 1, quality, blank);
 
+
+    // Probably should read in a text file with several preset building footprints and possibly an assoicated priority int
+    std::array<int,2> footprint = {20,10};
+    
+    /*int random_index = round(normals.size()*gaussianRoller(10));
+    std::array<std::array<int, 2>, 4> building = drawBuilding(random_index, footprint, normals, dimensions);
+
+    std::vector<std::array<int, 2>> building_data;
+    for (int s = 0; s < 4; s++) {
+        building_data.push_back(building[s]);
+    }
+    printPoints(building_data);*/
+
+    std::vector<std::array<int, 2>> building_data;
+    int number_of_buildings = 8;
+    building_data = drawManyBuildings(number_of_buildings, footprint, normals, dimensions);
+    blank = true;
+    arrayToImage(normals_image, building_data, dimensions, "testBuilding.jpg", 1, quality, blank);
+    
+
+    image = stbi_load("village_line.jpg", &width, &height, &channels, 1);
+    std::vector<std::array<int, 2>> original_line = getLineFull(image, dimensions);
+    std::vector<std::array<int, 2>> total_data;
+    for (int s = 0; s < original_line.size(); s++) {
+        total_data.push_back(original_line[s]);
+    }
+    for (int s = 0; s < building_data.size(); s++) {
+        total_data.push_back(building_data[s]);
+    }
+
+    blank = false;
+    arrayToImage(image, total_data, dimensions, "testManyBuildings.jpg", 1, quality, blank);
+    //blank = false;
+    //arrayToImage(image, building_data, dimensions, "testManyBuildings.jpg", 1, quality, blank);
 
 
     return 0;
 }
 
+// Graveyard:
+    // Prints particular image pixel's value:
+    /*int pixel_position[2] = {223,631};
+    getPixelPrint(image, pixel_position, dimensions)*/
 
 
+    /*uint8_t *processImage(uint8_t *img, int dims[]) {
+        int height,width;
+        width = dims[0];
+        height = dims[1];
+
+        for (int j = 0; j< height; j++) {
+            for (int i = 0; i<width; i++) {
+                if (img[j * width + i] < 128) {
+                    img[j * width + i] = 1;
+                } else {
+                    img[j * width + i] = 0;
+                }
+            }
+        }
+        return img;
+    }*/
+
+
+/*int getHits(uint8_t *img, int dims[]) {
+    // Unnecessary function, but useful if you have to initialize a vector with an explicit length
+
+    int height,width,hits;
+    width = dims[0];
+    height = dims[1];
+    hits = 0;
+
+    for (int j = 0; j< height; j++) {
+        for (int i = 0; i<width; i++) {
+            if (img[j * width + i] < 128) {
+                hits++;
+            }
+        }
+    }
+    return hits;
+}*/
